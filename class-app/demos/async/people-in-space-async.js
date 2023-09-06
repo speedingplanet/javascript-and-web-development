adf1b1be20c6389a95d485d3d7dfaf80451e68f9
John Paxton <johnpaxton@gmail.com>
2023-09-06 06:45:00 -0400

    Add people in space demo

---
+++ class-app/demos/async/people-in-space-async.js
@@ -0,0 +1,20 @@
+let url = 'http://api.open-notify.org/astros.json';
+
+try {
+	let response = await fetch(url);
+	let results;
+	if (response.ok) {
+		results = await response.json(); // Returns a Promise that resolves to a JS object
+	} else {
+		throw new Error(`Could not retrieve astronauts (data) ${response.status}`);
+	}
+	let list = document.querySelector('#astronaut-list');
+	let items = results.people.map((person) => {
+		let li = document.createElement('li');
+		li.textContent = `${person.name} on ${person.craft}`;
+		return li;
+	});
+	list.replaceChildren(...items);
+} catch (error) {
+	console.error('Could not fetch or render astronauts with regular fetch:', error.message);
+}
