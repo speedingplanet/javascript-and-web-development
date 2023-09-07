b8389616dce0c732d1b3f25210fc941f2257aabb
John Paxton <johnpaxton@gmail.com>
2023-09-07 14:39:25 -0400

    Arry to HTML example

--- class-app/demos/dom/array-to-table.js
+++ class-app/demos/dom/array-to-table.js
@@ -1,1 +1,30 @@
-// Placeholder
+import { movies } from '../../data/all-data-typed';
+
+/*
+for (let movie of movies) {
+	console.log(movie.title);
+}
+*/
+
+let outputDiv = document.querySelector('#output');
+
+function movieTitlesWithMap(movies, outputDiv) {
+	let titles = movies.map(movie => {
+		let p = document.createElement('p');
+		p.textContent = movie.title;
+		return p;
+	});
+
+	// ... is the spread operator
+	outputDiv.append(...titles);
+}
+
+function movieTitlesAsList(movies, outputDiv) {
+	let ul = document.createElement('ul');
+	for (let movie of movies) {
+		ul.insertAdjacentHTML('beforeend', `<li>${movie.title}</li>`);
+	}
+	outputDiv.replaceChildren(ul);
+}
+
+movieTitlesAsList(movies, outputDiv);
